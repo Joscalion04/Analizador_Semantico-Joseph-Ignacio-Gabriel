94d666bed2edf6368319ac50efaf31ab5163f4ef
NachoBonilla03 <ignacio.bonilla.rojas@est.una.ac.cr>
2023-11-08 19:33:25 -0600

    Reading files

--- Lectura.py
+++ Lectura.py
@@ -1,1 +1,5 @@
 #Lectura de archivos
+def read(archivo):
+    with open(archivo, 'r') as file:
+        lineas = file.readlines()
+    return lineas
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #Proyecto 2, Estructura de Datos, Ignacio Bonilla - Joseph Leon - Gabriel Robleto  
+import Lectura
+import Analisis
+
+
+lineas = Lectura.read("Codigo.txt")
+for linea in lineas: 
+    print(linea)
 
