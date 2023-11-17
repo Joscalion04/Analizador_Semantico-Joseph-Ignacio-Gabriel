cceab69cc71b0e16b7d189e61c6225599ebb25ad
NachoBonilla03 <ignacio.bonilla.rojas@est.una.ac.cr>
2023-11-16 23:36:57 -0600

    Advance at the lecture

--- main.py
+++ main.py
@@ -2,51 +2,14 @@
 import Lectura
 import Analisis
 
-dato = ""
-prev = "213a"
-text = None
-
-"""
-lineas = Lectura.read("Codigo.txt") 
-for linea in lineas: 
-    words = Lectura.separateWords(linea)
-    for word in words: 
-        if Analisis.isTipo(word) == True: 
-            dato = word
-        elif word == '=': 
-            prev = word
-        elif dato != None and prev != None: 
-            text = word
-            print(Analisis.tipoDato(dato,text))
-            dato = None
-            prev = None
-
-"""
-
-""""
-#Asi se agregan elementos en el diccionario 
-datos = {}     
-datos["x"] = (int, "122")  #asi agregamos variables 
-datos["y"] = (float, "122.5")
-
-funcion={}
-funcion["sumar"]=(10,10) #asi agregamos funciones 
-funcion["resta"]=(10,2)
-datos ["funcion"] = funcion
-#print(datos["funcion"]["resta"][1]) #asi se accede a un dato dentro de una posicion 
-
-linea = "double a = 12.3 "
-dato, nombre, valor = Lectura.dataIdentifyTypeToNameToValue(linea)
-print("El tipo de dato es:" + dato)
-print("El nombre de la variable es: "+ nombre)
-print("El valor es: "+valor)
-"""
-
 def lectura(linea, num, operacion, function, functionName):
     tipo = ""
     nombre = ""
     valor = ""
     error = "Linea " + str(num) + " exitosa"
+
+    
+
     try:
         tipo , nombre, valor, operacion = Lectura.dataIdentifyTypeToNameToValue(linea)
         if Analisis.isTipo(tipo) == False: 
@@ -59,14 +22,13 @@ def lectura(linea, num, operacion, function, functionName):
                 else:
                     error =  "Linea " + str(num) + " Error: "+ nombre + " es una variable que ya ha sido creada"
             else: 
-                if Analisis.KeyEnDiccionario(nombre)==False:
+                if Analisis.KeyInDiccionario(nombre)==False:
                     Analisis.addVariableGen(nombre,tipo)
                 else: 
                      error =  "Linea " + str(num) + " Error: "+ nombre + " es una variable que ya ha sido creada"
     except Exception: 
        print("linea sin poder leerse 1")
        pass
-
 
     try:
         tipo, nombre, operacion = Lectura.dataIdentifyTypeToName(line)
@@ -79,7 +41,7 @@ def lectura(linea, num, operacion, function, functionName):
                 else:
                     error =  "Linea " + str(num) + " Error: "+ nombre + " es una variable que ya ha sido creada"
             else: 
-                if Analisis.KeyEnDiccionario(nombre)==False:
+                if Analisis.KeyInDiccionario(nombre)==False:
                     Analisis.addVariableGen(nombre,tipo)
                 else: 
                      error =  "Linea " + str(num) + " Error: "+ nombre + " es una variable que ya ha sido creada"
@@ -89,7 +51,7 @@ def lectura(linea, num, operacion, function, functionName):
     
     try:
         nombre, valor, operacion = Lectura.dataIdentifyNameToValue(line)
-        if Analisis.KeyEnDiccionario(nombre) == True: 
+        if Analisis.KeyInDiccionario(nombre) == True: 
             if Analisis.validateGeneral(Analisis.TipoVarEnDiccionario(nombre),valor) == False:
                 error = "Linea " + str(num) + " Error: "+ str(valor) + " no es un dato aceptable para una variable de tipo " + str(tipo)
         elif function == True: 
@@ -102,8 +64,8 @@ def lectura(linea, num, operacion, function, functionName):
   
     try:
         nombre, valor, operacion = Lectura.dataIdentifyNameToName(line)
-        if Analisis.KeyEnDiccionario(nombre) == True: 
-            if Analisis.KeyEnDiccionario(valor) == True:
+        if Analisis.KeyInDiccionario(nombre) == True: 
+            if Analisis.KeyInDiccionario(valor) == True:
                 if Analisis.validateGeneral(Analisis.TipoVarEnDiccionario(nombre), Analisis.TipoVarEnDiccionario(valor)) == False:
                     error = "Linea " + str(num) + " Error: La variable " + str(nombre) + " y la variable " + str(valor) + " no son compatibles"
             elif function ==True:
@@ -114,7 +76,7 @@ def lectura(linea, num, operacion, function, functionName):
                 error = "Linea " + str(num) + " Error: La variable " + str(valor) + "  no ha sido declarada"
         elif function == True:
             if Analisis.KeyInFunction(functionName, nombre) == True:
-                if Analisis.KeyEnDiccionario(valor) == True:
+                if Analisis.KeyInDiccionario(valor) == True:
                     if Analisis.validateGeneral(Analisis.TipoValFuncionEnDiccionario(functionName,valor), Analisis.TipoVarEnDiccionario(valor)) == False:
                         error = "Linea " + str(num) + " Error: La variable " + str(nombre) + " y la variable " + str(valor) + " no son compatibles"
                 elif function ==True:
@@ -124,19 +86,73 @@ def lectura(linea, num, operacion, function, functionName):
                 else: 
                     error = "Linea " + str(num) + " Error: La variable " + str(valor) + "  no ha sido declarada"
         else : 
-            error = "Linea " + str(num) + " Error: La variable " + str(nombre) + "  no ha sido declarada"
-        
+            error = "Linea " + str(num) + " Error: La variable " + str(nombre) + "  no ha sido declarada"    
     except Exception:
         print("linea sin poder leerse 4")
         pass
+
+    try: #Incompleto falta agregar el que hacer con los  parametros 
+        tipo,nombre,parametros,operacion = Lectura.dataIdentifyWithParenthesis(line)
+        if Analisis.isTipo(tipo) == False: 
+            error = "Linea " + str(num) + " Error: "+ str(tipo) + " no es un tipo de dato aceptado"
+        elif Analisis.KeyInDiccionario(nombre)==True:
+            error =  "Linea " + str(num) + " Error: "+ nombre + " es una variable que ya ha sido creada"
+        else: 
+            Analisis.addFunctionGen(nombre,tipo)
+            function=True
+            functionName=nombre        
+    except Exception:
+        print("linea sin poder leerse 5")
+        pass
+    
+    if Lectura.find_closing_brace(line) == "8": 
+        function = False
+        functionName=None
+    else: 
+        print("linea sin poder leerse 6")
+
+    try: 
+        nombre, operations, operacion = Lectura.dataNametoOperation(line)
+        if Analisis.KeyInDiccionario(nombre) ==True:
+            tipo = Analisis.TipoVarEnDiccionario(nombre)
+            verificateTypes(tipo, operations, None, num)
+        elif function == True:
+            if Analisis.KeyInFunction(functionName, nombre):
+                tipo = Analisis.TipoValFuncionEnDiccionario(functionName, nombre)
+                verificateTypes(tipo, operations, functionName, num)
+                
+
+    except Exception:
+        print("linea sin poder leerse 7")
+        pass
+
     return error,operacion, function, functionName
 
+
+def verificateTypes(type, operations, functionName, lineNum):
+    for operation in operations:
+        if verificationTypeVar(type,operation,functionName, lineNum) != False:
+            if Analisis.validateGeneral(type, operation) == False:
+                print("Linea " + str(lineNum) + "Error: La variable" + str(operation) + " no es un valor valido")
+
+def verificationTypeVar(type, name, functionName, linenum): 
+    if Analisis.KeyInDiccionario(name): 
+        if type != Analisis.TipoVarEnDiccionario(name):
+            print("Linea " + str(linenum) + "Error: La variable" + str(name) + " no es un valor valido")
+            return False
+    elif functionName != None:
+        if Analisis.KeyInFunction(functionName,name) == True:
+            if type != Analisis.TipoValFuncionEnDiccionario(functionName, name): 
+                print("Linea " + str(linenum) + "Error: La variable" + str(name) + " no es un valor valido")
+                return False
+    else: 
+        return True        
 
 lineas = Lectura.read("Codigo.txt") 
 numLine = 1 
 operacion = ""
 function = False
-functionName = ""
+functionName = None
 for line in lineas: 
     error, operacion, function, functionName = lectura(line,numLine, operacion,function, functionName)
     print(error)
