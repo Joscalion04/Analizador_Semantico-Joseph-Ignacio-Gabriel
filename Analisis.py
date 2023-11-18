6912e3943af7eb0b19bb740107f8f0fbab9fbcbc
gabrielRobleto24 <gabriel.robleto.jerez@est.una.ac.cr>
2023-11-18 17:56:17 -0600

    Comentarios+actualizacion funcion verificateTypes

--- Analisis.py
+++ Analisis.py
@@ -1,24 +1,20 @@
 #Analisis Semantico
 import re
-
+#Diccionario con los tipos de variables permitidas 
 tiposDatos = {'int' :(), 'float':(), 'string': (), 'void':() }
+#Diccionario General 
 diccionarioGen = {}
 
-"""
-def tipoDato(word,dato): 
-    
-    if word in tiposDatos and isinstance(dato, tiposDatos[word]): 
-        return "word"+str(dato)
-    else: 
-        return "error"
-"""
 
+#Metodos encargados de verificar si un tipo es 
+#permitido o no, esto haciendo uso del diccionario tiposDatos
 def isTipo(word):
     if word in tiposDatos:
         return True
     else:  
         return False
 
+#Validates de los tipos de variable permitidas (int, float, string)
 def validateInt(value): 
     try: 
         int(value)
@@ -52,28 +48,58 @@ def validateGeneral(tipo, valor):
     else:
         return False
 
+
+#Funcion que con ayuda de la funcion verificationTypeVar y en 
+# base a un vector de nombres o valores
+#verifica que todos los datos del vector sean de un mismo 
+# tipo previamente declarado
 def verificateTypes(type, operations, functionName, lineNum):
     for operation in operations:
         if verificationTypeVar(type,operation,functionName, lineNum) == 3:
             if validateGeneral(type, operation) == False:
-                print("Linea " + str(lineNum) + "Error: La variable " + str(operation) + " no es un valor valido")
+                print("Linea " + str(lineNum) + " Error: La variable " + str(operation) + " no es un valor valido")
 
+#Verifica que el tipo de la variable es correcto, en caso de no ser una variable retorna un 3 para 
+# que el metodo verificateTypes verifique el tipo del valor 
 def verificationTypeVar(type, name, functionName, linenum): 
     if KeyInDiccionario(name): 
         if type != TipoVarEnDiccionario(name):
-            print("Linea " + str(linenum) + "Error: La variable" + str(name) + " no es un valor valido")
+            print("Linea " + str(linenum) + " Error: La variable " + str(name) + " no es un valor valido")
             return 1
         else:
            return 2
+    elif keyInIfGen(name)==True:
+        if type!=accesIfGenValues(name):
+            print ("Linea "+str(linenum)+" Error: La variable "+str(name)+" no es un valor valido")
+            return 1
+        else:
+            return 2
+    elif keyInWhileGen(name)==True:
+        if type!=accesWhileGenValues(name):
+            print ("Linea "+str(linenum)+" Error: La variable "+str(name)+" no es un valor valido")
+        else:
+            return 2
     elif functionName != None:
         if KeyInFunction(functionName,name) == True:
             if type != TipoValFuncionEnDiccionario(functionName, name): 
-                print("Linea " + str(linenum) + "Error: La variable" + str(name) + " no es un valor valido")
+                print("Linea " + str(linenum) + " Error: La variable " + str(name) + " no es un valor valido")
+                return 1 
+        elif keyInIfFunc(functionName,name)==True:
+            if type != accesIfFuncValues(functionName,name):
+                print ("Linea "+str(linenum)+" Error: La variable "+str(name)+" no es un valor valido")
                 return 1
-           
+            else: 
+                return 2
+        elif keyInWhileFunc(functionName,name)==True:
+            if type != accesWhileFuncValues(functionName,name):
+                print ("Linea "+str(linenum)+" Error: La variable "+str(name)+" no es un valor valido")
+                return 1
+            else: 
+                return 2
     else: 
         return 3        
 
+#Metodos para agregar datos al diccionario General
 def addVariableGen(name,type): 
     diccionarioGen[name]= ("var",type)
 # [0] es para "var", [1] para el tipo de variable
@@ -88,6 +114,7 @@ def addWhileGen():
     diccionarioGen["while"]=({})
 #[0] es para subdiccionario
 
+#Metodos para agregar datos a una funcion
 def addVarFunction(nameFun, nameVar, type): 
     diccionarioGen[nameFun][2][nameVar]= (type)
 
@@ -97,11 +124,13 @@ def addIfFunction(nameFun):
 def addWhileFunction(nameFun):
     diccionarioGen[nameFun][2]["while"]=({})
 
+#Metodos para agregar datos a los if o while que NO esten en una funcion
 def addVarIfGen(var,type):
     diccionarioGen["if"][var]=(type)
 def addVarWhileGen(var,type):
     diccionarioGen["while"][var]=(type)
 
+#Metodos para agregar datos a los if o while que estan en una funcion
 def addVarIfFun(funName,var,type):
     diccionarioGen[funName][2]["if"][var]=(type)
 
@@ -114,16 +143,19 @@ def accesIfGenValues(var):
     return diccionarioGen["if"][var]
 def accesWhileGenValues(var):
     return diccionarioGen["while"][var]
+
+#Accesos a valores de una variable dentro de una funcion y un if o while
 def accesIfFuncValues(funName,var):
     return diccionarioGen[funName][2]["if"][var]
 
 def accesWhileFuncValues(funName,var):
     return diccionarioGen[funName][2]["while"][var]
 
-
+#Metodo para añadir el tipo del return de una funcion
 def addReturnVal(nameFun): 
     diccionarioGen[nameFun][2]["return"]= (diccionarioGen[nameFun][1])
 
+#Metodo para verificar si una variable global o funcion esta en el diccionario
 def KeyInDiccionario(word):
     if word in diccionarioGen:
         return True
@@ -142,37 +174,48 @@ def AllSearch(word):
                 return True
     return False                
 
+#Metodo que verifica que una llave (nombre de una variable) este en una funcion
 def KeyInFunction(funName, key):
     if key in diccionarioGen[funName][2]:
         return True
     else:
         return False
 
+#Metodo que verifica que una llave (nombre de una variable) este en un If
 def keyInIfGen(key):
     if key in diccionarioGen["if"]:
        return True
     else:
         return False       
+    
+#Metodo que verifica que una llave (nombre de una variable) este en un while  
 def keyInWhileGen(key):
     if key in diccionarioGen["while"]:
         return True
     else:
         return False
 
+#Metodo que verifica que una llave (nombre de una variable) este en un If que esta dentro de 
+# una funcion
 def keyInIfFunc(funName,key):
     if key in diccionarioGen[funName][2]["if"]:
         return True
     else:
         return False
 
+#Metodo que verifica que una llave (nombre de una variable) este en un While que esta dentro de 
+#una funcion
 def keyInWhileFunc(funName,key):
     if key in diccionarioGen[funName][2]["while"]:
         return True
     else:
         return False
+
+#Metodo que devuelve el tipo de una variable
 def TipoVarEnDiccionario(key):
     return  diccionarioGen[key][1]
-    
+
+#Metodo que devuelve el tipo de una variable que esta dentro de una funncion
 def TipoValFuncionEnDiccionario(funName,key):
     return diccionarioGen[funName][2][key]
 
@@ -229,23 +272,37 @@ def insertValuesToFunction(nameFunction, values, numLine):
             print("Error en la linea "+str(numLine)+" El tipo de variable '" + tipo + "' no es aceptado\n")
             break
 
+
 #addVariableGen("a",'int')
 #addFunctionGen("sumar",'int')
-lista = ['int','b','a','string','valor','int','aux','32','float','nombre',"joseph"]
+#lista = ['int','b','x','int','c','int','o','32','string','nombre']
 #insertValuesToFunction("sumar",lista,1)
 
 
-#addVariableGen("uno","int")
+#addVariableGen("uno","string")
 #addVariableGen("dos","int")
 #addVariableGen("tres","int")
 
 #addFunctionGen("messi","void")
-#addVarFunction("messi","cuatro","string")
+#addVarFunction("messi","cuatro","int")
 
-Operations=["uno","dos","tres","cuatro"]
-#print(verificateTypes("string",Operations,"messi",2))
+#addIfGen()
+#addIfFunction("messi")
+
+#addVarIfGen("cinco","int")
+#addVarIfFun("messi","seis","int")
+#addWhileGen()
+#addWhileFunction("messi")
+
+#addVarWhileGen("siete","int")
+#addVarWhileFun("messi","ocho","string")
+
+
+
+#Operations=["uno","dos","tres","cuatro","cinco","seis","siete","ocho"]
+#verificateTypes("int",Operations,"messi",2)
 #print(TipoValFuncionEnDiccionario("messi","cuatro"))
-#print(validateString("'messigoat' fwgdgw "))
+#print(validateString('"pronar"   \t '))
 
 
     
